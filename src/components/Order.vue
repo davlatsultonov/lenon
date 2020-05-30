5eb857d4adcf2b5ad83de332db21b8f4209ae7d6
davlatsultonov <davlatsultonov2001@gmail.com>
2020-05-30 11:49:57 +0500

    добавил блок формы для заполнения персональных данных

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -11,9 +11,63 @@
 
             <div class="container">
 
-                <h1>
+                <h1 class="order__title">
                     Персональные данные
                 </h1>
+
+                <p class="order__text">
+                    Укажите ваши данные, для завершения формирования заказа
+                </p>
+
+                <form class="form-group">
+
+                    <div class="form-group__items-wrapper">
+                        <div class="form-group__item">
+
+                            <label class="form-group__label">Имя</label>
+
+                            <input type="text" class="form-group__input" placeholder="Елена">
+
+                        </div>
+
+                        <div class="form-group__item">
+
+                            <label class="form-group__label">Фамилия</label>
+
+                            <input type="text" class="form-group__input" placeholder="Иванова">
+
+                        </div>
+                    </div>
+
+                    <div class="form-group__items-wrapper">
+                        <div class="form-group__item">
+
+                            <label class="form-group__label">Телефон</label>
+
+                            <input type="tel" class="form-group__input" placeholder="+7 (000) 000-00-00">
+
+                            <div class="form-group__text">
+                                По нему с вами свяжется менеджер для подтверждения заказа
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group__items-wrapper">
+                        <div class="form-group__item">
+
+                            <label class="form-group__label">E-mail</label>
+
+                            <input type="email" class="form-group__input" placeholder="mail@mail.com">
+
+                            <div class="form-group__text">
+                                На него мы отправим информацию и статус заказа
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </form>
 
             </div>
 
